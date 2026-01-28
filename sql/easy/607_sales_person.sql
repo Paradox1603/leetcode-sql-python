df3d7a174d5729334745f967509d10f38a383e00
Arul Joe Kevin V <kvkevrosh16@gmail.com>
2026-01-28 18:05:36 +0530

    Add LC607, LC610: SQL solutions using CASE statements and sub-queries with easy difficulty

---
+++ sql/easy/607_sales_person.sql
@@ -0,0 +1,8 @@
+SELECT  name
+FROM    Salesperson 
+WHERE   sales_id NOT IN (  
+                        SELECT  sales_id
+                        FROM    Orders o, Company c
+                        WHERE   o.com_id = c.com_id
+                        AND     c.name = 'RED' 
+);
